0cd6929eff53ebe197bbef1f71073d9ccfe64cd2
Udo Eisenbarth <u.eisenbarth@gsi.de>
2023-05-31 16:23:03 +0200

    Add very simple "to_dot" function to opticscenery

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
+//! This is the documentation for the OPOSSUM software package. OPOSSUM stands for OPen-source Optics Simulation Software and Unified Modeller.
+//! 
+/// The basic structure containing the entire optical model
 pub mod opticscenery;
