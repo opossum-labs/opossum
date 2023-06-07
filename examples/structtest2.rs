0750abdc0821fa55b99817bdf101710d137ee3c6
Udo Eisenbarth <u.eisenbarth@gsi.de>
2023-06-07 13:30:45 +0200

    Add two examples for testing the overall node structure.

---
+++ examples/structtest2.rs
@@ -0,0 +1,61 @@
+use petgraph::prelude::{DiGraph, EdgeIndex, NodeIndex};
+trait Optical {
+  fn analyze(&self) {
+    println!("generic analyze");
+  }
+}
+struct OpticDummy;
+
+struct OpticIdealLens {
+  focal_length: f64,
+}
+
+impl OpticIdealLens {
+  pub fn new(focal_length: f64) -> Self {
+    Self{focal_length}
+  }
+}
+
+impl Optical for OpticIdealLens {
+  fn analyze(&self) {
+      println!("ideal lens analyze: f={}",self.focal_length);
+  }
+}
+enum NodeType {
+  Dummy,
+  IdealLens(OpticIdealLens),
+  SimpleElement(f64)
+}
+impl NodeType {
+  fn analyze(&self) {
+    match self {
+        NodeType::Dummy => println!("dummy -> nothing to do here"),
+        NodeType::IdealLens(n) => n.analyze(),
+        _ => println!("not covered")
+    }
+  }
+}
+struct OpticNode {
+  name: String,
+  node: NodeType
+}
+
+impl OpticNode {
+  fn new(name: &str, node_type: NodeType) -> Self {
+    Self{name: name.into(), node: node_type}
+  }
+  fn analyze(&self) {
+    print!("Analyze {}: ",self.name);
+    self.node.analyze();
+  }
+}
+fn main() {
+  let node=OpticNode::new("Test1",NodeType::Dummy);
+  node.analyze();
+  let node=OpticNode::new("Test2",NodeType::IdealLens(OpticIdealLens::new(1.23)));
+  node.analyze();
+  let node=OpticNode::new("Test2",NodeType::SimpleElement(1.23));
+  node.analyze();
+
+  let p:DiGraph<OpticNode,()> = DiGraph::new();
+}
