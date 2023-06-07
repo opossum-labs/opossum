0357287b523ce8c798645807d5f4b74a3bf35048
Udo Eisenbarth <u.eisenbarth@gsi.de>
2023-06-07 14:35:54 +0200

    Improve "add" functions for OpticScenery & OpticNode

--- examples/opticscenery.rs
+++ examples/opticscenery.rs
@@ -1,5 +1,4 @@
 use opossum::nodes::NodeDummy;
-use opossum::optic_node::OpticNode;
 use opossum::optic_scenery::OpticScenery;
 
 use std::fs::File;
@@ -11,8 +10,8 @@ fn main() {
     scenery.set_description("OpticScenery demo".into());
     println!("default opticscenery: {:?}", scenery);
     println!("export to `dot` format: {}", scenery.to_dot());
-    let node1 = scenery.add_node(OpticNode::new("my optic", Box::new(NodeDummy)));
-    let node2 = scenery.add_node(OpticNode::new("my other optic", Box::new(NodeDummy)));
+    let node1 = scenery.add_element("my optic", NodeDummy);
+    let node2 = scenery.add_element("my other optic", NodeDummy);
     if let Ok(_) = scenery.connect_nodes(node1, node2) {
         let path = "graph.dot";
         let mut output = File::create(path).unwrap();
