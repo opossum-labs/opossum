66d1e2e9dc1d5d191becdfe4f03440ee2a84383b
Udo Eisenbarth <udo.eisenbarth@web.de>
2023-06-13 21:31:35 +0200

    Work on reference node (not yet final solution)

--- src/nodes/node_reference.rs
+++ src/nodes/node_reference.rs
@@ -1,17 +1,19 @@
+use std::rc::{Weak, Rc};
+
 use crate::optic_node::{OpticNode, Optical};
 
 /// A virtual component referring to another existing component. This node type is necessary in order to model resonators (loops) or double-pass systems.
-pub struct NodeReference<'a> {
-    reference: &'a OpticNode,
+pub struct NodeReference {
+    reference: Weak<OpticNode>,
 }
 
-impl<'a> NodeReference<'a> {
-    pub fn new(node: &'a OpticNode) -> Self {
-        Self { reference: node }
+impl NodeReference {
+    pub fn new(node: OpticNode) -> Self {
+        Self { reference: Rc::downgrade(&Rc::new(node)) }
     }
 }
 
-impl<'a> Optical for NodeReference<'a> {
+impl Optical for NodeReference {
     fn node_type(&self) -> &str {
         "reference"
     }
