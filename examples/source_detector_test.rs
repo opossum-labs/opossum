94d3be461cdd15b578c108185aa38266926c6232
Udo Eisenbarth <udo.eisenbarth@web.de>
2023-07-09 13:37:02 +0200

    Add bream splitter to src-detector demo

--- examples/source_detector_test.rs
+++ examples/source_detector_test.rs
@@ -3,7 +3,7 @@ use std::io::Write;
 
 use opossum::{
     lightdata::{LightData, LightDataEnergy},
-    nodes::{NodeDetector, NodeSource},
+    nodes::{NodeDetector, NodeSource, NodeBeamSplitter},
     optic_scenery::OpticScenery, analyzer::AnalyzerEnergy,
 };
 
@@ -15,18 +15,26 @@ fn main() {
         "Source",
         NodeSource::new(LightData::Energy(LightDataEnergy { energy: 1.0 })),
     );
-    let i_d = scenery.add_element("Detector", NodeDetector::default());
+    let i_bs=scenery.add_element("Beam splitter", NodeBeamSplitter::default());
+    let i_d1 = scenery.add_element("Detector 1", NodeDetector::default());
+    let i_d2 = scenery.add_element("Detector 2", NodeDetector::default());
 
-    scenery.connect_nodes(i_s, "out1", i_d, "in1").unwrap();
+    scenery.connect_nodes(i_s, "out1", i_bs, "input1").unwrap();
 
+    scenery.connect_nodes(i_bs, "out1_trans1_refl2", i_d1, "in1").unwrap();
+    scenery.connect_nodes(i_bs, "out2_trans2_refl1", i_d2, "in1").unwrap();
+    
     let path = "src_detector.dot";
     let mut output = File::create(path).unwrap();
     write!(output, "{}", scenery.to_dot()).unwrap();
     println!("{:?}", scenery.node_ref(i_s).unwrap());
-    println!("{:?}", scenery.node_ref(i_d).unwrap());
+    println!("{:?}", scenery.node_ref(i_d1).unwrap());
 
     let mut analyzer=AnalyzerEnergy::new(&scenery);
-    analyzer.analyze();
-    println!("Analyze...");
-    println!("{:?}", scenery.node_ref(i_d).unwrap());
+    print!("Analyze...");
+    match analyzer.analyze() {
+        Ok(_) => println!("Sucessful"),
+        Err(e) => println!("Error: {}",e)
+    }
+    println!("{:?}", scenery.node_ref(i_d1).unwrap());
 }
