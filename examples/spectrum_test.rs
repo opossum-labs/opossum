a0d928846b0abe8f9a74b7b15d00d598752f9c23
Udo Eisenbarth <udo.eisenbarth@web.de>
2023-07-19 17:59:00 +0200

    Improve spectrum example with sub function

--- examples/spectrum_test.rs
+++ examples/spectrum_test.rs
@@ -1,14 +1,13 @@
 use opossum::error::OpossumError;
 use opossum::spectrum::Spectrum;
-use uom::si::{f64::Length, length::{nanometer,meter}};
+use uom::si::{f64::Length, length::meter};
 
 fn main() -> Result<(), OpossumError> {
     let mut s = Spectrum::new(
         Length::new::<meter>(400.0)..Length::new::<meter>(450.0),
         Length::new::<meter>(0.1),
     )?;
-    //s.add_single_peak(Length::new::<meter>(405.0), 1.0)?;
-    s.add_lorentzian_peak(Length::new::<meter>(410.0), Length::new::<meter>(0.2), 1.0)?;
+    s.add_lorentzian_peak(Length::new::<meter>(415.0), Length::new::<meter>(3.2), 2.0)?;
 
     let mut s2=Spectrum::new(
         Length::new::<meter>(400.0)..Length::new::<meter>(450.0),
@@ -16,6 +15,13 @@ fn main() -> Result<(), OpossumError> {
     )?;
     s2.add_lorentzian_peak(Length::new::<meter>(430.0), Length::new::<meter>(1.2), 0.5)?;
     s.add(&s2);
+
+    let mut s3=Spectrum::new(
+        Length::new::<meter>(400.0)..Length::new::<meter>(450.0),
+        Length::new::<meter>(0.05),
+    )?;
+    s3.add_lorentzian_peak(Length::new::<meter>(420.0), Length::new::<meter>(0.3), 0.02)?;
+    s.sub(&s3);
     s.to_plot("spectrum.svg");
     Ok(())
 } 
