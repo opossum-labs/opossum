2edbfbef693a0aa8ab36084f83dd0aab86e933b1
y.zobus <y.zobus@gsi.de>
2023-09-20 15:52:02 +0200

    simple console prompt

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //! This is the documentation for the **OPOSSUM** software package. **OPOSSUM** stands for
 //! **Op**en-source **O**ptics **S**imulation **S**oftware and **U**nified **M**odeller.
 //!
@@ -25,28 +24,5 @@ pub mod spectrum;
 pub mod properties;
 
 pub use optic_scenery::OpticScenery;
-=======
-//! This is the documentation for the **OPOSSUM** software package. **OPOSSUM** stands for
-//! **Op**en-source **O**ptics **S**imulation **S**oftware and **U**nified **M**odeller.
-//!
-mod light;
-pub mod lightdata;
-/// The basic structure representing an optical element
-pub mod optic_node;
-/// The basic structure containing the entire optical model
-mod optic_scenery;
-
-pub mod optic_ports;
-
-pub mod nodes;
-
-pub mod analyzer;
-
-pub mod error;
-
-pub mod spectrum;
-
-pub use optic_scenery::OpticScenery;
 
 pub mod console;
->>>>>>> dde6804 (simple console prompt)
