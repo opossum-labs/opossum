2446520ab091f25d47e777e37c196ec65a29c589
y.zobus <y.zobus@gsi.de>
2023-09-25 14:01:57 +0200

    added logo suggestions + intro text for cli

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use opossum::{console::{Args, PartialArgs}, error::OpossumError};
+use opossum::{{
+    console::{Args, PartialArgs, show_intro}, 
+    error::OpossumError}
+};
 use clap::Parser;
 type Result<T> = std::result::Result<T, OpossumError>;
 
 
 
 fn main() -> Result<()>{    
+    //not necessary, just for fun
+    show_intro();
     let opossum_args = Args::try_from(PartialArgs::parse())?;
 
     println!("file path: {}", opossum_args.file_path);
