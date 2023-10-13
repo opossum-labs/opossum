9db9a05e8c74cfd3e6d68013be50f49807d00b68
Udo Eisenbarth <u.eisenbarth@gsi.de>
2023-10-13 10:09:49 +0200

    Add unit tests for Spectrometer::analyze

--- src/nodes/spectrometer.rs
+++ src/nodes/spectrometer.rs
@@ -53,6 +53,7 @@ fn create_default_props() -> Properties {
         "spectrometer type",
         SpectrometerType::IdealSpectrometer.into(),
     );
+    props.set("inverted", false.into());
     props
 }
 impl Default for Spectrometer {
@@ -97,6 +98,9 @@ impl Optical for Spectrometer {
     }
     fn node_type(&self) -> &str {
         "spectrometer"
+    }
+    fn inverted(&self) -> bool {
+        self.properties().get_bool("inverted").unwrap().unwrap()
     }
     fn ports(&self) -> OpticPorts {
         let mut ports = OpticPorts::new();
@@ -214,16 +218,61 @@ mod test {
         assert_eq!(meter.ports().outputs(), vec!["out1"]);
     }
     #[test]
-    fn analyze() {
-        let mut meter = Spectrometer::default();
+    fn inverted() {
+        let mut node = Spectrometer::default();
+        node.set_property("inverted", true.into()).unwrap();
+        assert_eq!(node.inverted(), true)
+    }
+    #[test]
+    fn analyze_ok() {
+        let mut node = Spectrometer::default();
         let mut input = LightResult::default();
-        input.insert(
-            "in1".into(),
-            Some(LightData::Energy(DataEnergy {
-                spectrum: create_he_ne_spectrum(1.0),
-            })),
-        );
-        let result = meter.analyze(input, &AnalyzerType::Energy);
-        assert!(result.is_ok());
+        let input_light = LightData::Energy(DataEnergy {
+            spectrum: create_he_ne_spectrum(1.0),
+        });
+        input.insert("in1".into(), Some(input_light.clone()));
+        let output = node.analyze(input, &AnalyzerType::Energy);
+        assert!(output.is_ok());
+        let output = output.unwrap();
+        assert!(output.contains_key("out1".into()));
+        assert_eq!(output.len(), 1);
+        let output = output.get("out1".into()).unwrap();
+        assert!(output.is_some());
+        let output = output.clone().unwrap();
+        assert_eq!(output, input_light);
+    }
+    #[test]
+    fn analyze_wrong() {
+        let mut node =Spectrometer::default();
+        let mut input = LightResult::default();
+        let input_light = LightData::Energy(DataEnergy {
+            spectrum: create_he_ne_spectrum(1.0),
+        });
+        input.insert("wrong".into(), Some(input_light.clone()));
+        let output = node.analyze(input, &AnalyzerType::Energy);
+        assert!(output.is_ok());
+        let output = output.unwrap();
+        let output = output.get("out1".into()).unwrap();
+        assert!(output.is_none());
+    }
+    #[test]
+    fn analyze_inverse() {
+        let mut node = Spectrometer::default();
+        node.set_property("inverted", true.into()).unwrap();
+        let mut input = LightResult::default();
+        let input_light = LightData::Energy(DataEnergy {
+            spectrum: create_he_ne_spectrum(1.0),
+        });
+        input.insert("out1".into(), Some(input_light.clone()));
+
+        let output = node.analyze(input, &AnalyzerType::Energy);
+        assert!(output.is_ok());
+        let output = output.unwrap();
+        assert!(output.contains_key("in1".into()));
+        assert_eq!(output.len(), 1);
+        let output = output.get("in1".into()).unwrap();
+        assert!(output.is_some());
+        let output = output.clone().unwrap();
+        assert_eq!(output, input_light);
     }
 }
