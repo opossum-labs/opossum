80415b3c26ba8cd016b85518508d572348b3ee12
y.zobus <y.zobus@gsi.de>
2024-02-08 12:47:18 +0100

    removed source report from optic scenery

--- opossum/src/optic_scenery.rs
+++ opossum/src/optic_scenery.rs
@@ -350,21 +350,6 @@ impl OpticScenery {
             node.optical_ref.borrow().export_data(report_dir)?;
         }
 
-        let source_nodes = self
-            .g
-            .0
-            .node_weights()
-            .filter(|node| node.optical_ref.borrow().is_source());
-        let ray_source_bundles = Vec::<Rays>::new();
-        for node in source_nodes {
-            //create report for sources
-            //must check if this is a raytrace analysis! otherwise this does not make sense!
-            if let Some(node_report) = node.optical_ref.borrow().report() {
-                analysis_report.add_detector(node_report);
-            }
-
-            // node.optical_ref.borrow().export_data(report_dir)?;
-        }
         Ok(analysis_report)
     }
     /// Save this [`OpticScenery`] to an .opm file with the given path
