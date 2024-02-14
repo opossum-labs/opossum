63a90069825bc211ac2fc1127877df167bee0d32
Udo Eisenbarth <u.eisenbarth@gsi.de>
2024-02-14 17:35:30 +0100

    feat: :sparkles: Add new distribution stragey: regular grid.

--- opossum/src/distribution.rs
+++ opossum/src/distribution.rs
@@ -15,16 +15,19 @@ pub enum DistributionStrategy {
     Random(usize),
     /// Square, low-discrepancy quasirandom distribution with a given number of points within a given side length
     Sobol(usize),
+    /// Square, evenly sized grid with the given number of points
+    Grid(usize)
 }
 
 impl DistributionStrategy {
-    /// Generate a vector of 2D points within a given size (which depends on the concrete strategy)
+    /// Generate a vector of 2D points within a given size (which depends on the concrete [`DistributionStrategy`])
     #[must_use]
     pub fn generate(&self, size: Length) -> Vec<Point3<Length>> {
         match self {
             Self::Hexapolar(rings) => hexapolar(*rings, size),
             Self::Random(nr_of_rays) => random(*nr_of_rays, size),
             Self::Sobol(nr_of_rays) => sobol(*nr_of_rays, size),
+            Self::Grid(nr_of_rays) => grid(*nr_of_rays, size)
         }
     }
 }
@@ -74,6 +77,17 @@ fn sobol(nr_of_rays: usize, side_length: Length) -> Vec<Point3<Length>> {
     }
     points
 }
+fn grid(nr_of_rays: usize, side_length: Length) -> Vec<Point3<Length>> {
+    let distance=side_length/((nr_of_rays-1) as f64);
+    let offset=side_length/2.0;
+    let mut points: Vec<Point3<Length>> = Vec::new();
+    for i_x in 0..nr_of_rays {
+        for i_y in 0..nr_of_rays {
+            points.push(Point3::new((i_x as f64)*distance-offset, (i_y as f64)*distance-offset,Length::zero()))
+        }
+    }
+    points
+}
 #[cfg(test)]
 mod test {
     use super::*;
@@ -86,5 +100,15 @@ mod test {
     fn strategy_sobol() {
         let strategy = DistributionStrategy::Sobol(10);
         assert_eq!(strategy.generate(Length::new::<millimeter>(1.0)).len(), 10);
+    }
+    #[test]
+    fn strategy_grid() {
+        let strategy = DistributionStrategy::Grid(2);
+        let points=strategy.generate(Length::new::<millimeter>(1.0));
+        assert_eq!(points.len(),4);
+        assert_eq!(points[0], Point3::new(Length::new::<millimeter>(-0.5), Length::new::<millimeter>(-0.5), Length::zero()));
+        assert_eq!(points[1], Point3::new(Length::new::<millimeter>(-0.5), Length::new::<millimeter>(0.5), Length::zero()));
+        assert_eq!(points[2], Point3::new(Length::new::<millimeter>(0.5), Length::new::<millimeter>(-0.5), Length::zero()));
+        assert_eq!(points[3], Point3::new(Length::new::<millimeter>(0.5), Length::new::<millimeter>(0.5), Length::zero()));
     }
 }
