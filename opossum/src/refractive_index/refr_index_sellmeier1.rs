9da22fbe01358185fda57caace6795338f5fdec4
Udo Eisenbarth <u.eisenbarth@gsi.de>
2024-03-06 17:25:36 +0100

    feat: :sparkles: Add first support for refractive index dispersion functions.
    Added trivial const and Sellmeier1 functions.

--- opossum/src/refractive_index/refr_index_sellmeier1.rs
+++ opossum/src/refractive_index/refr_index_sellmeier1.rs
@@ -2,6 +2,9 @@ use serde::Deserialize;
 use serde::Serialize;
 use uom::si::length::micrometer;
 
+use crate::error::OpmResult;
+use crate::error::OpossumError;
+
 use super::{RefractiveIndex, RefractiveIndexType};
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
@@ -14,16 +17,31 @@ pub struct RefrIndexSellmeier1 {
     l3: f64,
 }
 impl RefrIndexSellmeier1 {
-    #[must_use]
-    pub const fn new(k1: f64, k2: f64, k3: f64, l1: f64, l2: f64, l3: f64) -> Self {
-        Self {
+    pub fn new(k1: f64, k2: f64, k3: f64, l1: f64, l2: f64, l3: f64) -> OpmResult<Self> {
+        if !k1.is_finite() || !k2.is_finite() || !k3.is_finite() {
+            return Err(OpossumError::Other(
+                "all k coefficients must be finite".into(),
+            ));
+        }
+        if l1.is_sign_negative()
+            || !l1.is_finite()
+            || l2.is_sign_negative()
+            || !l2.is_finite()
+            || l3.is_sign_negative()
+            || !l3.is_finite()
+        {
+            return Err(OpossumError::Other(
+                "all l coefficients must be positive and finite.".into(),
+            ));
+        }
+        Ok(Self {
             k1,
             k2,
             k3,
             l1,
             l2,
             l3,
-        }
+        })
     }
 }
 impl RefractiveIndex for RefrIndexSellmeier1 {
@@ -38,5 +56,23 @@ impl RefractiveIndex for RefrIndexSellmeier1 {
     }
     fn to_enum(&self) -> RefractiveIndexType {
         RefractiveIndexType::Sellmeier1(self.clone())
+    }
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn new() {
+        assert!(RefrIndexSellmeier1::new(1.0, 1.0, 1.0, 1.0, 1.0, -1.0).is_err());
+        assert!(RefrIndexSellmeier1::new(1.0, 1.0, 1.0, 1.0, 1.0, f64::NAN).is_err());
+        assert!(RefrIndexSellmeier1::new(1.0, 1.0, 1.0, 1.0, 1.0, f64::INFINITY).is_err());
+
+        assert!(RefrIndexSellmeier1::new(1.0, 1.0, 1.0, 1.0, -1.0, 1.0).is_err());
+        assert!(RefrIndexSellmeier1::new(1.0, 1.0, 1.0, 1.0, f64::NAN, 1.0).is_err());
+        assert!(RefrIndexSellmeier1::new(1.0, 1.0, 1.0, 1.0, f64::INFINITY, 1.0).is_err());
+
+        assert!(RefrIndexSellmeier1::new(1.0, 1.0, 1.0, -1.0, 1.0, 1.0).is_err());
+        assert!(RefrIndexSellmeier1::new(1.0, 1.0, 1.0, f64::NAN, 1.0, 1.0).is_err());
+        assert!(RefrIndexSellmeier1::new(1.0, 1.0, 1.0, f64::INFINITY, 1.0, 1.0).is_err());
     }
 }
