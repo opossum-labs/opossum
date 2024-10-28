1a253d5f407a592767903866bc01e3a1336e8371
y.zobus <y.zobus@gsi.de>
2024-10-28 13:18:29 +0100

    changed example and updated talk

--- opossum/examples/ghost_focus.rs
+++ opossum/examples/ghost_focus.rs
@@ -5,22 +5,20 @@ use opossum::{
     error::OpmResult,
     joule, millimeter,
     nodes::{
-        collimated_line_ray_source, round_collimated_ray_source, Lens, NodeGroup, SpotDiagram,
+        round_collimated_ray_source, Lens, NodeGroup, SpotDiagram,
         Wedge,
     },
-    optic_node::{Alignable, OpticNode, LIDT},
+    optic_node::{Alignable, OpticNode},
     optic_ports::PortType,
     refractive_index::RefrIndexConst,
-    J_per_cm2, OpmDocument,
+    OpmDocument,
 };
 use std::path::Path;
 
 fn main() -> OpmResult<()> {
     let mut scenery = NodeGroup::default();
     let i_src = scenery.add_node(
-        // collimated_line_ray_source(millimeter!(50.), joule!(1.), 100)?
-        &round_collimated_ray_source(millimeter!(50.0), joule!(50.0), 15)?
-            .with_lidt(J_per_cm2!(20.)),
+        &round_collimated_ray_source(millimeter!(50.0), joule!(2.0), 10)?            
     )?;
     let i_sd = scenery.add_node(&SpotDiagram::default())?;
 
@@ -32,7 +30,6 @@ fn main() -> OpmResult<()> {
     let mut lens2 = Lens::default();
     lens2.set_coating(&PortType::Input, "front", &CoatingType::Fresnel)?;
     lens2.set_coating(&PortType::Output, "rear", &CoatingType::Fresnel)?;
-    let i_l2 = scenery.add_node(&lens2)?;
 
     let mut wedge = Wedge::new(
         "wedge",
@@ -43,18 +40,13 @@ fn main() -> OpmResult<()> {
     .with_tilt(degree!(0.5, 0.0, 0.0))?;
     wedge.set_coating(&PortType::Input, "front", &CoatingType::Fresnel)?;
     wedge.set_coating(&PortType::Output, "rear", &CoatingType::Fresnel)?;
-    let i_w = scenery.add_node(&wedge)?;
 
-    let i_sd2 = scenery.add_node(&SpotDiagram::default())?;
-    scenery.connect_nodes(i_src, "out1", i_l, "front", millimeter!(100.0))?;
-    scenery.connect_nodes(i_l, "rear", i_l2, "front", millimeter!(1000.0))?;
-    // scenery.connect_nodes(i_l2, "rear", i_w, "front", millimeter!(100.0))?;
-    // scenery.connect_nodes(i_w, "rear", i_sd, "in1", millimeter!(50.0))?;
+    scenery.connect_nodes(i_src, "out1", i_l, "front", millimeter!(110.0))?;
+    scenery.connect_nodes(i_l, "rear", i_sd, "in1", millimeter!(150.0))?;
 
     let mut doc = OpmDocument::new(scenery);
     let mut config = GhostFocusConfig::default();
     config.set_max_bounces(2);
     doc.add_analyzer(AnalyzerType::GhostFocus(config));
-    // doc.add_analyzer(AnalyzerType::RayTrace(RayTraceConfig::default()));
     doc.save_to_file(Path::new("./opossum/playground/ghost_focus.opm"))
 }
