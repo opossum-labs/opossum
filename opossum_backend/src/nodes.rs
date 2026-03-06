0dfc07785bfa4ee6bc042a12dd4f7fa50383b7c8
y.zobus <y.zobus@gsi.de>
2026-03-06 15:48:44 +0100

    used with_node_attr_node_mut in post_node_name

--- opossum_backend/src/nodes.rs
+++ opossum_backend/src/nodes.rs
@@ -508,21 +508,12 @@ async fn post_node_name(
 ) -> Result<(), BackEndErrorResponse> {
     let uuid: Uuid = path.into_inner();
     let name = name.into_inner();
-    let document = data.document.lock();
-    if let Ok((node_ref, _)) = document.scenery().node_recursive(uuid) {
-        node_ref
-            .optical_ref
-            .lock_opm()?
-            .node_attr_mut()
-            .set_name(&name);
-        Ok(())
-    } else {
-        Err(BackEndErrorResponse::new(
+    let mut document = data.document.lock();
+    document.scenery_mut().with_node_attr_node_mut(uuid, |node_attr| node_attr.set_name(&name)).map_err(|_|BackEndErrorResponse::new(
             404,
             "Opossum",
             "uuid not found in nodes",
         ))
-    }
 }
 /// Update the laser-induced damage threshold (LIDT) of an optical node
 #[utoipa::path(tag = "node",
