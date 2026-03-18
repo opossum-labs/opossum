93a06dc73393fce6db64ce6fa877219c93ed3a83
y.zobus <y.zobus@gsi.de>
2026-03-18 11:17:12 +0100

    feat: :sparkles: Graph is now dragged using the middle mouse button

--- opossum_gui/src/components/scenery_editor/graph_editor/hooks.rs
+++ opossum_gui/src/components/scenery_editor/graph_editor/hooks.rs
@@ -64,13 +64,16 @@ pub fn use_on_mouse_down(
                 MouseButton::Primary => {
                     let mut ctx = CONTEXT_MENU.write();
                     *ctx = None;
+                }
+                MouseButton::Auxiliary => {
+                    //for dragging
                     current_mouse_pos.set(Point2D::new(
                         event.client_coordinates().x,
                         event.client_coordinates().y,
                     ));
                     editor_status.write().drag_status.set(DragStatus::Graph);
-                }
-                MouseButton::Auxiliary => {
+                    
+                    //for double-click zoom
                     event.stop_propagation();
                     let now = Instant::now();
                     let t0_opt = *last_click.read();
